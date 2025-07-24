492efc43f0181d8ac090245ca32820e207240927
se-zero <jso8831@naver.com>
2025-07-24 17:17:11 +0900

    파일 수정

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,51 @@
+import 'dotenv/config'; //.env 파일의 환경변수를 불러올 수 있게 해줌
+
 export default {
   expo: {
     name: "tagging-memory",
     slug: "tagging-memory",
+    version: "1.0.0",
+    orientation: "portrait",  
+    icon: "./assets/images/icon.png",
+    scheme: "taggingmemory",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true,  //태블릿 지원여부
+    },
+
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+    },
+
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+
+    experiments: {
+      typedRoutes: true,
+    },
+
     extra: {
       firebaseApiKey: process.env.FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
